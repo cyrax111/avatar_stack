783f26447e6c7551077093248bd1f754cbea257a
cyrax <bskaxe@gmail.com>
2023-03-24 18:31:45 +0400

    feat: add tests

--- test/bloc/positions_test.dart
+++ test/bloc/positions_test.dart
@@ -8,4 +8,29 @@ void main() {
       throwsAssertionError,
     );
   });
+
+  group('ItemPosition -', () {
+    final item = ItemPosition(number: 2, x: 10.0, y: 0, size: 10.0);
+
+    test('toString', () {
+      expect(item.toString, returnsNormally);
+    });
+
+    test('hashCode', () {
+      expect(() => item.hashCode, returnsNormally);
+    });
+  });
+
+  group('InfoItemPosition -', () {
+    final item = InfoItemPosition(
+        number: 2, x: 10.0, y: 0, size: 10.0, amountAdditionalItems: 4);
+
+    test('toString', () {
+      expect(item.toString, returnsNormally);
+    });
+
+    test('hashCode', () {
+      expect(() => item.hashCode, returnsNormally);
+    });
+  });
 }
